67b3c0d1964f7c59765c29112d89213cd90be62b
ambujdubey <ambujdubey89@gmail.com>
2020-06-27 21:44:24 +0530

    start work on angular app with .net restfull api's

---
+++ DotNetDatingApp.api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+using DotNetDatingApp.api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetDatingApp.api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options){}
+        public DbSet<Value> Values { get; set; }
+    }
+}
