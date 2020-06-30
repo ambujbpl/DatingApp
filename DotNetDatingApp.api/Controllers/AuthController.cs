15ce2339e2f042b32423cd87d8cec5030fdccc3c
ambujdubey <ambujdubey89@gmail.com>
2020-06-30 09:58:14 +0530

    user Auth, jwt token, and authorization

--- DotNetDatingApp.api/Controllers/AuthController.cs
+++ DotNetDatingApp.api/Controllers/AuthController.cs
@@ -1,20 +1,27 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DotNetDatingApp.api.Data;
 using DotNetDatingApp.api.Dtos;
 using DotNetDatingApp.api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DotNetDatingApp.api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController :ControllerBase
+    public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _repo;
-        public AuthController(IAuthRepository repo)
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository repo, IConfiguration config)
         {
+            _config = config;
             _repo = repo;
-
         }
 
         [HttpPost("register")]
@@ -22,14 +29,49 @@ namespace DotNetDatingApp.api.Controllers
         {
             // validation request
             userForRegisterDto.username = userForRegisterDto.username.ToLower();
-            if(await _repo.UserExists(userForRegisterDto.username))
+            if (await _repo.UserExists(userForRegisterDto.username))
                 return BadRequest("user name already exist");
             var userToCreate = new User
             {
                 userName = userForRegisterDto.username
             };
-            var createdUser = await _repo.Register(userToCreate,userForRegisterDto.password);
+            var createdUser = await _repo.Register(userToCreate, userForRegisterDto.password);
             return StatusCode(201);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(UserForLoginDto userForLoginDto)
+        {
+            var userFromRepo = await _repo.Login(userForLoginDto.username.ToLower(), userForLoginDto.password);
+            if (userFromRepo == null)
+                return Unauthorized();
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, userFromRepo.id.ToString()),
+                new Claim(ClaimTypes.Name, userFromRepo.userName)
+            };
+            
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return Ok(new {
+                token = tokenHandler.WriteToken(token)
+            });
+            
         }
     }
 }
--- DotNetDatingApp.api/Dtos/UserForRegisterDto.cs
+++ DotNetDatingApp.api/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DotNetDatingApp.api.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string username { get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage="You Must specify Password between 4 and 8 charactors")]
         public string password { get; set; }
     }
 }
