89e133026d1af5e80b7447452d06fa749203348d
ambujdubey <ambujdubey89@gmail.com>
2020-07-14 17:02:56 +0530

    add plugins like alertify, ngx bootstrap angular jwt and bootswatch on the application

--- AngularDatingApp/src/app/nav/nav.component.ts
+++ AngularDatingApp/src/app/nav/nav.component.ts
@@ -28,8 +28,9 @@ export class NavComponent implements OnInit {
 
   // tslint:disable-next-line: typedef
   loggedIn() {
-    const token = localStorage.getItem('token');
-    return !!token;
+    // const token = localStorage.getItem('token');
+    // return !!token;
+    return this.authService.loggedIn();
   }
 
   // tslint:disable-next-line: typedef
