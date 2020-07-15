bd3914ee8a8ae16c229b713ac8142916da05b264
ambujdubey <ambujdubey89@gmail.com>
2020-07-15 20:01:56 +0530

    update user table in asp.net and add photos table which mapped with user table

--- DotNetDatingApp.api/Models/User.cs
+++ DotNetDatingApp.api/Models/User.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 namespace DotNetDatingApp.api.Models
 {
     public class User
@@ -6,5 +9,16 @@ namespace DotNetDatingApp.api.Models
         public string userName { get; set; }
         public byte[] passwordHash { get; set; }
         public byte[] passwordSalt { get; set; }
+        public string gender { get; set; }
+        public DateTime dateOfBirth { get; set; }
+        public string knownAs { get; set; }
+        public DateTime created { get; set; }
+        public DateTime lastActive { get; set; }
+        public string introduction { get; set; }
+        public string lookingFor { get; set; }
+        public string interests { get; set; }
+        public string city { get; set; }
+        public string country { get; set; }
+        public ICollection<Photo> photos { get; set; }
     }
 }
---
+++ DotNetDatingApp.api/Models/photo.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DotNetDatingApp.api.Models
+{
+    public class Photo
+    {
+        public int id { get; set; }
+        public string url { get; set; }
+        public string description { get; set; }
+        public DateTime dateadded { get; set; }
+        public bool ismain { get; set; }
+        public User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
