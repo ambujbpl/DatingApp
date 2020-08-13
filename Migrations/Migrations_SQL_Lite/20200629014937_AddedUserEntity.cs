c996eeb6fce6ef561961abb4f18e5b9eab0934e8
ambujdubey <ambujdubey89@gmail.com>
2020-08-13 10:27:04 +0530

    update documents for production build and some core end points

---
+++ Migrations/Migrations_SQL_Lite/20200629014937_AddedUserEntity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotNetDatingApp.api.Migrations
+{
+    public partial class AddedUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        // .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    userName = table.Column<string>(nullable: true),
+                    passwordHash = table.Column<byte[]>(nullable: true),
+                    passwordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
